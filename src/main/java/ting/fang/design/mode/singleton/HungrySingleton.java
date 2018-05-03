84a2b7f413cfd63ee2d445c279e5c8e76a9d75a2
ting.fang <ting.fang@dianping.com>
2018-05-03 13:15:07 +0800

    all

---
+++ src/main/java/ting/fang/design/mode/singleton/HungrySingleton.java
@@ -0,0 +1,7 @@
+package ting.fang.design.mode.singleton;
+
+/**
+ * Created by fang on 2018/5/3.
+ */
+public class HungrySingleton {
+}
---
+++ src/main/java/ting/fang/design/mode/singleton/LazySingleton.java
@@ -0,0 +1,24 @@
+package ting.fang.design.mode.singleton;
+
+/**
+ * Created by fang on 2018/5/3.
+ */
+public class LazySingleton {
+
+    private volatile static LazySingleton instance = null;
+
+    private LazySingleton() {
+
+    }
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
